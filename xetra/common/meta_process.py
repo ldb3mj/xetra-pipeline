72461f966ad59245d2a0993138821c5727e4e71f
Landon Blakey <ldb3mj@virginia.edu>
2021-12-15 12:03:35 -0500

    added list_files_in_prefix and unittest

---
+++ xetra/common/meta_process.py
@@ -0,0 +1,20 @@
+"""
+Methods for processing the meta file
+"""
+
+from os import stat
+
+
+class MetaProcess():
+    """
+    class for working with the meta file
+    """
+
+    @staticmethod
+    def update_meta_file():
+        pass
+
+    @staticmethod
+    def return_date_list():
+        pass
+    
